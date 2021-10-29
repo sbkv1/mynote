9c3dba97f6833e9829fbb313415228e8ea0d7611
sbkv1 <93185814+sbkv1@users.noreply.github.com>
2021-10-29 19:15:24 +0530

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,101 @@
+module.exports = function(mainWindow){
+    
+    const template = [
+        {
+         label: 'File',
+         submenu: [
+            {
+                label:"New File",
+                click () { mainWindow.webContents.send('new-file'); }
+            },
+            {
+               label: 'Open File',
+               click () { mainWindow.webContents.send('open-file'); }
+            },
+            {
+               type: 'separator'
+            },
+            {
+               role: 'close'
+            },
+         ]
+      },
+      {
+         label: 'Edit',
+         submenu: [
+            {
+               role: 'undo'
+            },
+            {
+               role: 'redo'
+            },
+            {
+               type: 'separator'
+            },
+            {
+               role: 'cut'
+            },
+            {
+               role: 'copy'
+            },
+            {
+               role: 'paste'
+            }
+         ]
+      },
+      
+      {
+         label: 'View',
+         submenu: [
+            {
+                label: 'Clear Recents',
+                click () { mainWindow.webContents.send('clear-recents', 'Hello World!'); }
+             },
+            {
+               role: 'reload'
+            },
+            {
+               role: 'toggledevtools'
+            },
+            {
+               type: 'separator'
+            },
+            {
+                label: 'Toggle Recents',
+                click () { mainWindow.webContents.send('toggle-recents', 'Hello World!'); }
+            },
+            {
+               type: 'separator'
+            },
+            {
+               role: 'togglefullscreen'
+            }
+         ]
+      },
+      
+      {
+         role: 'window',
+         submenu: [
+            {
+               role: 'minimize'
+            },
+            {
+               role: 'close'
+            }
+         ]
+      },
+      
+      {
+         role: 'help',
+         submenu: [
+            {
+               label: 'Learn More',
+               click () { require('electron').shell.openExternal('https://github.com/wyntonfranklin/electron-tutorial') }
+            }
+         ]
+      }
+    ]
+
+    return template;
+
+}
